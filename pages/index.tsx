87f27f527b3bded8aa96e8a4a658e4ae240d02c7
timothyshen <timothyshen1@hotmail.com>
2023-11-18 00:24:27 +0300

    add worldcoin login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import WalletConnectButton from '../components/Web3Modal'
+import WorldCoinButton from '../components/WorldCoinButton'
 
 export default function Home() {
   return (
     <div className="w-[400px] h-[600px] flex flex-col items-center justify-center gap-4 bg-gray-200">
       <WalletConnectButton />
-      <button className="px-6 py-3 bg-green-500 text-white font-semibold rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300">
-        Button 2
-      </button>
+
+      <WorldCoinButton />
     </div>
   )
 }
